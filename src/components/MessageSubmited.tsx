c5dd68c7ce46a803d3809fbc3b13fbe5b65ca4d1
PevaarSF <pevaarsoftware@gmail.com>
2025-03-21 17:10:00 -0500

    botón iniciar nueva solicitud

--- src/components/MessageSubmited.tsx
+++ src/components/MessageSubmited.tsx
@@ -4,7 +4,7 @@ interface Props {
   iniciarNuevaSolicitud: () => void
 }
 
-export const MessageSubmited = ({ iniciarNuevaSolicitud }: Props) => {
+export const MessageSubmited = ({  }: Props) => {
 
   return (
     <div className="py-8 flex justify-center items-center flex-col gap-2">
@@ -13,7 +13,7 @@ export const MessageSubmited = ({ iniciarNuevaSolicitud }: Props) => {
       <p className='font-light text-gray-400'>Se ha enviado la peticion a recursos humanos y lideres de area para aprobación.</p>
       <button 
         className='className="w-full mt-5 px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-offset-gray-800 transition-colors duration-300"'
-        onClick={() => iniciarNuevaSolicitud }
+        onClick={() => window.location.reload() }
       >
           Enviar otra solicitud
         </button>
